218ad5e0a761a79ede94f1f0d1659fdacf0ed5e4
minampa <minampa.ghorbani@gmail.com>
2022-01-19 23:42:35 +0330

    Faal Page Almost Ready. Just mptURL remins

--- src/components/Fal.js
+++ src/components/Fal.js
@@ -6,10 +6,14 @@ const Fal= ({title, plainText, htmlText}) => (
         <div className='fal'>
             <div className='fal-info'>
                 <h3>{title}</h3>
-                {/* <div>{plainText}</div> */}
-                <div id="poemContainer"></div>
+                    <div id="poemContainer"></div>
                 <div className='fal-tabir'>
-                    ای صاحب فال به زودی خبر خوشی دریافت خواهی کرد. فقط جناب حافظ می‌فرمایند نماز اول وقت فراموش نشود :D
+                    <p><b>:ای صاحب فال </b></p>
+                    <p>به زودی خبرهای خوشی دریافت خواهی کرد. هر کجا داخل شعر می و ساقی و مطرب و بوس و کنار و آغوش و... بود، منظور قرب وجودی به درگاه حق تعالی است! حرف هم نباشه جناب حافظ می‌فرمایند نماز اول وقت هم فراموش نشود</p>
+                </div>
+                <div className='btn-fal'>
+                    <button className='btn'>بازگشت به صفحه اصلی</button>
+                    <button className='btn'>فال مجدد</button>
                 </div>
             </div>
         </div>
